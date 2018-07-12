8eb5582b16af49c3e47c42dca91616541f618d3e
novazhang613 <zhyiqun@umich.edu>
2018-07-12 16:51:42 -0400

    Add "LIM_ADDR_OFFSET"

--- simulator/cpu/recryptor/recryptor.h
+++ simulator/cpu/recryptor/recryptor.h
@@ -21,6 +21,8 @@ typedef enum _recryptor_op {
 
 void recryptor_decoder_wr(uint32_t addr, uint32_t val, bool debugger __attribute__ ((unused)) ); 
 
+extern const int LIM_ADDR_OFFSET;    
+ 
 extern const uint8_t NUM_SUBBANK[];
 extern const uint8_t NUM_PREVTOT_SUBBANK[];
 extern const char    *OpNames[]; 
